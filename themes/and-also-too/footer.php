dfdc07b4abb63d026c84f57a2a129d1044c558fe
Darren Puscas <darren@darrenpuscas.com>
2017-02-11 21:53:01 -0500

    Header and Footer

--- themes/and-also-too/footer.php
+++ themes/and-also-too/footer.php
@@ -13,31 +13,43 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer std-content-area" role="contentinfo">
-		<div class="footer-middle">
-			<div class="footer-logo">
-				<div class="logo-img">Logo Here</div>
-			</div>
-			<div class="footer-social-medium">
-				<i class="fa fa-twitter-square" aria-hidden="true"></i>
-				<i class="fa fa-facebook-square" aria-hidden="true"></i>
-				<i class="fa fa-instagram" aria-hidden="true"></i>
-				<i class="fa fa-linkedin-square" aria-hidden="true"></i>
-			</div>
-		</div>
-		<div class="footer-phone">
-			<div class="contact-phone">Canada (416)628-8452</div>
-			<div class="contact-phone">USA (802)730 0091</div>
-		</div>
-		<div class="footer-social">
-			<i class="fa fa-twitter-square" aria-hidden="true"></i>
-			<i class="fa fa-facebook-square" aria-hidden="true"></i>
-			<i class="fa fa-instagram" aria-hidden="true"></i>
-			<i class="fa fa-linkedin-square" aria-hidden="true"></i>
-		</div>
-		<div class="footer-email">
-			<div class="email-text">collborate@andalsotoo.net</div>
-			</div>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="outer-content">
+				<div class="footer-content">
+					<div class="footer-middle">
+						<div class="footer-logo">
+							<div class="logo-img">
+								<img src="http://and-also-too.dev/wp-content/uploads/2017/01/Logo-1-e1485882114317.png" alt="">
+							</div>
+						</div>
+						<div class="footer-logo-medium">
+							<div class="logo-img">
+								<img src="http://and-also-too.dev/wp-content/uploads/2017/02/logo-vertical.png" alt="">
+							</div>
+						</div>
+						<div class="footer-social-medium">
+							<i class="fa fa-twitter-square" aria-hidden="true"></i>
+							<i class="fa fa-facebook-square" aria-hidden="true"></i>
+							<i class="fa fa-instagram" aria-hidden="true"></i>
+							<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="footer-phone">
+						<div class="contact-phone">Canada (416)628-8452</div>
+						<div class="contact-phone">USA (802)730 0091</div>
+					</div>
+					<div class="footer-email">
+						<div class="email-text">collborate@andalsotoo.net</div>
+					</div>
+					<div class="footer-social">
+						<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						<i class="fa fa-facebook-square" aria-hidden="true"></i>
+						<i class="fa fa-instagram" aria-hidden="true"></i>
+						<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+					</div>
+
+				</div><!-- .footer-content-->
+		</div><!-- .outer-content-->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
