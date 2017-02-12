a4b6639b2de8c0454df1a52f42c35deb62d79089
MichaelLSmith <mlsmith45@gmail.com>
2017-02-12 11:31:33 -0500

    basic tax

--- themes/and-also-too/template-parts/stories-content-acf.php
+++ themes/and-also-too/template-parts/stories-content-acf.php
@@ -13,27 +13,22 @@
 	<header class="">
 		<div class="story-featured-image">
 			<?php
-			if ( has_post_thumbnail() ) {
-			    the_post_thumbnail();
-			}
-			?>
-		</div><!-- .featured-image -->
+			if ( has_post_thumbnail() ) :
+			    the_post_thumbnail();?>
 
-		<?php
-			if( has_post_thumbnail() ):
-		?>
-		<div class="story-title">
-			<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-				else :
-					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-				endif;
-				?>
-			<h6 class="">with</h6>
-			<h6 class=""><?php the_field('sub-heading_for_title')?></h6>
-			<!-- catie, The Teresa Group, Womens College Hospital -->
-		</div><!-- .story-title -->
+				<div class="story-title">
+					<?php
+					if ( is_single() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+						else :
+							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+						endif;
+						?>
+						<h6 class="">with</h6>
+						<h6 class=""><?php the_field('sub-heading_for_title')?></h6>
+						<!-- catie, The Teresa Group, Womens College Hospital -->
+					</div><!-- .story-title -->
+		</div><!-- .featured-image -->
 
 	<?php else: ?>
 
@@ -48,7 +43,7 @@
 				?>
 			<h6 class="">with</h6>
 			<h6 class=""><?php the_field('sub-heading_for_title')?></h6>
-			<!-- catie, The Teresa Group, Womens College Hospital -->
+			<!--  -->
 		</div>
 	<?php endif;?>
 
@@ -60,7 +55,6 @@
 			<div class="story-left-col">
 				<h1 class=""><?php the_title( '<h1 class="entry-title">', '</h1>' );?></h1>
 
-				<h6 class="">the need</h6>
 				<?php the_field('story_main_text_section')?>
 			</div><!-- .story-left-col -->
 			<aside class="story-right-col">
@@ -80,61 +74,56 @@
 					</h4>
 					<div class="story-tags-section">
 						<h5>communities</h5>
-						<ul>
-							<li>HIV/AIDS</li>
-							<li>Montreal</li>
-							<li>Regina</li>
-							<li>Toronto</li>
-						</ul>
+						<?php the_terms( $post->ID, 'tax-community', '<ul class="story-tax"><li>', '</li><li>', '</li></ul>'); ?>
+
 					</div>
 					<div class="story-tags-section">
 						<h5>services</h5>
-						<ul>
-							<li>Art Design</li>
-							<li>Co-design</li>
-							<li>Layout</li>
-						</ul>
+						<?php the_terms( $post->ID, 'tax-service', '<ul class="story-tax"><li>', '                        </li><li>', '</li></ul>'); ?>
 					</div>
 					<div class="story-tags-section">
 						<h5>credits</h5>
-						<h6>Art Direction</h6>
-						<ul>
-							<li>Una Lee</li>
-						</ul>
-						<h6>Design Interns</h6>
-						<ul>
-							<li>Faduma Gure</li>
-							<li>Lara Stefanovich-Thomson</li>
-						</ul>
-						<h6>Illustration</h6>
-						<ul>
-							<li>Joanna Lawniczak</li>
-						</ul>
-						<h6>Community Partners</h6>
-						<ul>
-							<li>CATIE</li>
-							<li>The Teresa Group</li>
-							<li>Womens College Hospital</li>
-						</ul>
-						</div>
+					<?php
+
+						// check if the repeater field has rows of data
+						if( have_rows('story_credits') ):
+							// loop through the rows of data
+							while ( have_rows('story_credits') ) : the_row();
+							    // display a sub field value
+								$terms = get_sub_field('name_or_partner');
+					?>
+								<h6><?php the_sub_field('story_credit_title');?></h6>
+								<?php
+									if( $terms ): ?>
+										<ul>
+											<?php foreach( $terms as $term ): ?>
+												<li><a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></li>
+											<?php endforeach; ?>
+										</ul>
+									<?php endif; ?>
+							<?php endwhile;
+
+						else :
+						    // no rows found
+						endif;
+								?>
+					</div><!-- story-tags-section -->
 				</div><!-- .story-tags -->
 			</aside><!-- story-right-col -->
 		</section><!-- story-2col-flex- -->
 	    <section class="story-image-gallery">
-			<!-- Note: I used .alignleft and .alignright on the images because that's what WP uses automatically if you align images in the editor in the admin area. But it doesn't work on mobile because the two images don't re-align one ontop of each other. I guess we'll have to use an ACF gallery and not allow such an image layout in the admin editor -->
-				<?php
+			<?php
 
-					$images = get_field('story_image_gallery');
-					if( $images ): ?>
-					        <?php foreach( $images as $image ): ?>
-					            <div class="story-gallery-image">
-					                <a href="<?php echo $image['url']; ?>">
-					                     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-					                </a>
-					            </div>
-					        <?php endforeach; ?>
-					<?php endif; ?>
-
+				$images = get_field('story_image_gallery');
+				if( $images ): ?>
+				        <?php foreach( $images as $image ): ?>
+				            <div class="story-image-in-gallery">
+				                <a href="<?php echo $image['url']; ?>">
+				                     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+				                </a>
+				            </div>
+				        <?php endforeach; ?>
+				<?php endif; ?>
 	    </section>
 	    <section class="story-secondary-content">
 		    <p>
@@ -144,5 +133,10 @@
 			   Thou dankish onion-eyed clotpole! Thou paunchy dread-bolted giglet! Thou currish common-kissing boar-pig! Thou churlish flap-mouthed fustilarian!
 		   </p>
 	    </section>
+
+
+
+
+
     </div><!-- .entry-content -->
 </article><!-- #post-## -->
