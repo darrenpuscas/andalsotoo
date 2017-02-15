819a57f6cb7ac7ca4cf30c9427c0a89eb5783e9e
MichaelLSmith <mlsmith45@gmail.com>
2017-02-15 17:21:39 -0500

    stories-archive working

--- themes/and-also-too/archive-stories.php
+++ themes/and-also-too/archive-stories.php
@@ -27,23 +27,8 @@ get_header(); ?>
 				<?php
 				if ( have_posts() ) :
 
-					// if ( is_home() && ! is_front_page() ) : ?>
-						<!-- <header> -->
-							<!-- <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1> -->
-						<!-- </header> -->
-
-					<?php
-
-					// endif;
-
 					/* Start the Loop */
 					while ( have_posts() ) : the_post();
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
 						get_template_part( 'template-parts/stories-content-from-static', get_post_format() );
 
 					endwhile; ?>
@@ -56,7 +41,6 @@ get_header(); ?>
 					get_template_part( 'template-parts/content', 'none' );
 
 				endif; ?>
-		<!-- move to archive-stories.php -->
 			</div><!-- .outer-content-->
 		</div><!-- .entry-content -->
 	</main><!-- #main -->
