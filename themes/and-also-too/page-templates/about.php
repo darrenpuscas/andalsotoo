72ae1a8f54863f444356325adfc6bc783d2e775b
Darren Puscas <darren@darrenpuscas.com>
2017-02-19 16:38:58 -0500

    About-mobile

--- themes/and-also-too/page-templates/about.php
+++ themes/and-also-too/page-templates/about.php
@@ -35,7 +35,20 @@ get_header(); ?>
 						</section><!-- .front-content about-full-width -->
 
 
+						<div class="bg-about-heading-mobile">
+							<div class="overlay-about-heading-mobile">
+									<h1 class="about-content-heading-mobile">About </h1>
+									<h1 class="about-content-title-mobile">and also too </h1>
+									<div class="about-content-text-mobile">is a collaborative design studio. We work with social justice visionaries and use community centered design processes to make beautiful, powerful things.
+									</div>
+							</div><!-- .overlay-about-heading-mobile-->
 
+						</div><!-- .bg-about-heading-mobile-->
+
+
+						<div class="hhh">
+								<img src="http://and-also-too.dev/wp-content/uploads/2017/02/about-image-1.png" alt="" />
+						</div>
 
 
 					<div class="outer-content-about-mobile about-flex">
@@ -45,10 +58,10 @@ get_header(); ?>
 								&nbsp;
 						</section>
 
-						<section class="about-group1 about-grid-item">
 
+
+						<section class="about-group1 ddd about-grid-item">
 									<h1 class="about-title-main-mobile outer-content">About</h1>
-
 							<div class="shift-title-1">and also too </div>
 							<div class="shift-text-1">is a collaborative design studio. We work with social justice visionaries and use community centered design processes to make beautiful, powerful things.</div>
 							<div class="shift-image-1">
@@ -58,6 +71,7 @@ get_header(); ?>
 								</div>
 							</div>
 						</section>
+
 
 
 						<section class="about-group-2 about-grid-item">
@@ -76,27 +90,39 @@ get_header(); ?>
 							<div class="shift-icon-2-medium">
 								<img src="http://and-also-too.dev/wp-content/uploads/2017/02/icon2-1.png" alt="" />
 							</div>
-							<div class="shift-title-3">...and use community centered design processess</div>
+							<div class="shift-title-3">...and use community centered design processes</div>
 							<div class="shift-text-3">We facilitate groups of people connected by choice or circumstance to co-design the future. We belive that people are experts in their own experiences, that everyone has the ability to participate in creative processes, and so we design with and not for communities experiencing injustice.</div>
 						</section>
+
+
+
+
+
+						<div class="hhh">
+							<div class=about-grid-item">
+								<img src="http://and-also-too.dev/wp-content/uploads/2017/02/about-image-3.png" alt="" />
+							</div>
+						</div>
+
+
+
+
 
 						<section class="about-group4 about-grid-item">
 							<div class="shift-icon-3-medium">
 								<img src="http://and-also-too.dev/wp-content/uploads/2017/02/icon1-3.png" alt="" />
 							</div>
-							<div class="about-grid-item">
+							<div class="about-grid-item non-mobile4">
 								<div class="shift-title-4">...to make beautiful, powerful things.</div>
 								<div class="shift-text-4">Our task is to help shine light on the issues and uplift the answers that the community holds. We draw inspiration from the community's culture and history to co-create resources and tools that are unique, accessible, and delightful.
 									<div class="shift-title-5">want to collaborate?</div>
 									<div class="about-button">Get in touch</div>
-							</div>
-
-
-
+								</div>
 
 							</div>
 
 						</section>
+
 
 						<section class="about-group5 about-grid-item">
 							<div class="shift-image-3 about-grid-item">
@@ -105,17 +131,36 @@ get_header(); ?>
 						</section>
 
 
-
 					</div><!-- .outer-content-->
+
+
 					<div class="about-flex eee">
 							<section class="about-full-width about-full-width2">
-									<div class="bg-about-1">
+									<div class="bg-about-2">
 										<div class="overlay-about-2">
 										</div><!-- .overlay-about-1-->
 									</div><!-- .bg-about-1-->
 							</section><!-- .front-content about-full-width -->
 					</div>
+
+
+					<div class="bg-about-heading-mobile-2">
+						<div class="overlay-about-heading-mobile">
+							<div class="about-grid-item">
+								<div class="shift-title-4">...to make beautiful, powerful things.ddddddd</div>
+								<div class="shift-text-4">Our task is to help shine light on the issues and uplift the answers that the community holds. We draw inspiration from the community's culture and history to co-create resources and tools that are unique, accessible, and delightful.
+									<div class="shift-title-5">want to collaborate?</div>
+									<div class="about-button">Get in touch</div>
+							</div>
+
+
+						</div><!-- .overlay-about-heading-mobile-->
+
+					</div><!-- .bg-about-heading-mobile-->
+
 				</div><!-- .entry-content -->
+
+
 
 			</article><!-- #post-## -->
 
